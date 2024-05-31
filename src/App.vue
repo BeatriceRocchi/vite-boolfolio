5f7698b4fd4d471a9f60716d45d68546fff9aa08
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-05-31 14:51:42 +0200

    [add] API to endpoint of Laravel project and [print] result data in console

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {};
+  },
+
+  methods: {
+    getApi() {
+      axios
+        .get("http://127.0.0.1:8000/api/projects")
+        .then((result) => {
+          console.log(result.data);
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    },
+  },
+  mounted() {
+    this.getApi();
+  },
+};
 </script>
 
 <template>
