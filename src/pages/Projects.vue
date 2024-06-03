0616a16ee98702ac3c1e4b0fbadb525c0284f75b
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-06-03 16:22:19 +0200

    [add] Api to get all projects types and technologies

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -17,19 +17,33 @@ export default {
     return {
       store,
       projects: [],
+      types: [],
+      technologies: [],
       paginatorData: {},
     };
   },
 
   methods: {
-    getApi(apiUrl) {
+    getApi(apiUrl, type = "") {
       axios
-        .get(apiUrl)
+        .get(apiUrl + type)
         .then((result) => {
-          this.projects = result.data.data;
-          this.paginatorData.current_page = result.data.current_page;
-          this.paginatorData.links = result.data.links;
-          this.paginatorData.last_page = result.data.last_page;
+          switch (type) {
+            case "types":
+              this.types = result.data;
+              break;
+
+            case "technologies":
+              this.technologies = result.data;
+              break;
+
+            default:
+              this.projects = result.data.data;
+              this.paginatorData.current_page = result.data.current_page;
+              this.paginatorData.links = result.data.links;
+              this.paginatorData.last_page = result.data.last_page;
+              break;
+          }
         })
         .catch((error) => {
           console.log(error.message);
@@ -37,13 +51,40 @@ export default {
     },
   },
   mounted() {
-    this.getApi(store.apiUrl + "projects");
+    this.getApi(store.apiUrl, "projects");
+    this.getApi(store.apiUrl, "types");
+    this.getApi(store.apiUrl, "technologies");
   },
 };
 </script>
 
 <template>
   <h2>My projects</h2>
+
+  <div class="container d-flex justify-content-around my-4">
+    <div class="text-center">
+      <h6>Types</h6>
+      <span
+        class="badge badge-custom text-bg-primary"
+        v-for="item in types"
+        :key="`type-${item.id}`"
+      >
+        {{ item.name }}
+      </span>
+    </div>
+
+    <div class="text-center">
+      <h6>Technologies</h6>
+      <span
+        class="badge badge-custom text-bg-success"
+        v-for="item in technologies"
+        :key="`tech-${item.id}`"
+      >
+        {{ item.name }}
+      </span>
+    </div>
+  </div>
+
   <div class="card-wrapper">
     <div class="container">
       <div class="row row-gap-4">
@@ -62,6 +103,10 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+.badge-custom {
+  margin: 0 5px;
+}
+
 .card-wrapper {
   display: flex;
   flex-wrap: wrap;
