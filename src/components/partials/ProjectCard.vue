58e2d006549c699ca5b7c1dfb13e58f63be134e3
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-06-04 18:20:03 +0200

    [add] effect on card hover

--- src/components/partials/ProjectCard.vue
+++ src/components/partials/ProjectCard.vue
@@ -9,31 +9,43 @@ export default {
 <template>
   <div class="col">
     <div class="card card-custom">
-      <div class="card-body">
-        <h5 class="card-title">{{ projectObject.title }}</h5>
-        <p class="card-text">
-          {{ projectObject.description }}
-        </p>
+      <div class="card-custom-front">
+        <div class="card-body">
+          <h5 class="card-title">{{ projectObject.title }}</h5>
+          <p class="card-text">
+            {{ projectObject.description }}
+          </p>
 
-        <div>
-          <span class="badge text-bg-primary">
-            {{ projectObject.type?.name }}
+          <div>
+            <span class="badge text-bg-primary">
+              {{ projectObject.type?.name }}
+            </span>
+          </div>
+
+          <span
+            v-for="technology in projectObject.technologies"
+            :key="technology.id"
+            class="badge text-bg-success me-1"
+          >
+            {{ technology.name }}
           </span>
         </div>
-
-        <span
-          v-for="technology in projectObject.technologies"
-          :key="technology.id"
-          class="badge text-bg-success me-1"
-        >
-          {{ technology.name }}
-        </span>
       </div>
-      <router-link
-        :to="{ name: 'projectDetail', params: { slug: projectObject.slug } }"
-        class="btn btn-custom"
-        >More details</router-link
-      >
+
+      <div class="card card-custom-back">
+        <div
+          class="card-body d-flex justify-content-center align-items-center h-100"
+        >
+          <router-link
+            :to="{
+              name: 'projectDetail',
+              params: { slug: projectObject.slug },
+            }"
+            class="btn btn-custom"
+            >More details</router-link
+          >
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -45,14 +57,43 @@ export default {
   min-width: 300px;
 
   .card-custom {
-    min-height: 250px;
+    min-height: 350px;
+    position: relative;
 
-    .btn-custom {
-      background-color: $color-secondary;
-      width: fit-content;
-      margin: 10px;
-      align-self: flex-end;
+    &-back {
+      display: none;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      padding: 10px;
+      top: 0;
+      left: 0;
+      overflow-y: auto;
+
+      .btn-custom {
+        background-color: white;
+        width: fit-content;
+      }
     }
+
+    &:hover {
+      .card-custom-back {
+        animation: 0.9s expand ease-in-out;
+        display: block;
+        background-color: $color-primary;
+      }
+    }
+  }
+}
+
+@keyframes expand {
+  0% {
+    transform: scale(0);
+    background-color: rgba($color-primary, 0.2);
+  }
+  100% {
+    transform: scale(1);
+    background-color: $color-primary;
   }
 }
 </style>
